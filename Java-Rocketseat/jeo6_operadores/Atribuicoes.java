193a5af6c27efa78d5edd0b6aef6a9384026a02c
Sophya <148731999+devsophya@users.noreply.github.com>
2024-12-10 11:54:16 -0300

    jeo6_operadores - concluído com êxito.
    jeo6_operadores - concluído com êxito.

---
+++ Java-Rocketseat/jeo6_operadores/Atribuicoes.java
@@ -0,0 +1,16 @@
+package jeo6_operadores;
+
+public class Atribuicoes {
+    public static void main(String[] args) {
+        String nome = "Sophya" + " Damiazo";
+        System.out.println(nome);
+    }
+}
+/*
+int idade = 19;
+double peso = 79.8;
+char sexo = 'F';
+boolean doadorOrgao = false;
+Date dataNascimento = new Date();
+
+ */
