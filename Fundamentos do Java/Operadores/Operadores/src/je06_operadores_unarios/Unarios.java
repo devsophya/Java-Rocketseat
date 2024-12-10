c66440dbea8390e3838f3d6db06e1a93a835f3d7
Sophya <148731999+devsophya@users.noreply.github.com>
2024-12-10 13:03:45 -0300

    je06_operadores_unarios - concluído com êxito.
    je06_operadores_unarios - concluído com êxito.

---
+++ Fundamentos do Java/Operadores/Operadores/src/je06_operadores_unarios/Unarios.java
@@ -0,0 +1,24 @@
+package je06_operadores_unarios;
+
+public class Unarios {
+    public static void main(String[] args) {
+        boolean falsa = false;
+        if(!falsa)
+            System.out.println("False");
+
+        //boolean falsa = ! (1==1);
+        //boolean falsa = !verdadeira;
+
+       // System.out.println(falsa);
+    }
+}
+
+/*
+        int numero = 5;
+        //numero++;
+        System.out.println(++numero);
+
+        System.out.println(-numero);
+        System.out.println(numero);
+
+ */
