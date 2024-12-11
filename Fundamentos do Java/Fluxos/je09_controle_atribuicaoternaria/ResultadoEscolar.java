0f03e5e25308c7f4dea1c9574f48c90c67d21850
Sophya <148731999+devsophya@users.noreply.github.com>
2024-12-11 16:02:48 -0300

    je09_controle_atribuicaoternaria - concluído com êxito.
    je09_controle_atribuicaoternaria - concluído com êxito.

---
+++ Fundamentos do Java/Fluxos/je09_controle_atribuicaoternaria/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+package je09_controle_atribuicaoternaria;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+        String resultado = nota >=7 ? "Aprovado!" : nota >=5 && nota<7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+    static void fluxoIntermediario(){
+        System.out.println("Recuperação");
+        System.out.println("Estude um pouco mais!");
+    }
+}
+}
