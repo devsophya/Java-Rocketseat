a0ba3248f6a3943ab3b2295a404e4be0c2f27275
Sophya <148731999+devsophya@users.noreply.github.com>
2024-12-14 17:58:07 -0300

    ExemploDoWhile - concluído com êxito.
    ExemploDoWhile - concluído com êxito.

---
+++ Fundamentos do Java/Fluxos/je10_controle_fluxo_repeticao/ExemploDoWhile.java
@@ -0,0 +1,27 @@
+package je10_controle_fluxo_repeticao;
+
+import java.util.Random;
+
+public class ExemploDoWhile {
+    private static int numeroTentativas =0;
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+        do {
+            System.out.println("Telefone tocando - trim trimmm");
+        }while (tocando());
+        if (numeroTentativas<5)
+            System.out.println("Alô !!!");
+        else
+            System.out.println("Não atendeu... ");
+    }
+
+    private static boolean tocando() {
+        numeroTentativas++;
+        boolean atendeu = new Random().nextInt(5)==1;
+        if (numeroTentativas>=5)
+            return false;
+        else
+            return ! atendeu;
+    }
+
+}
