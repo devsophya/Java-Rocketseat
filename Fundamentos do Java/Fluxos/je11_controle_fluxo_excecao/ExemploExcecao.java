52b6e40b8cf57ad270cb0f916f8969ba4ae23188
Sophya <148731999+devsophya@users.noreply.github.com>
2024-12-17 10:19:02 -0300

    je11_controle_fluxo_excecao - concluído com êxito.
    je11_controle_fluxo_excecao - concluído com êxito.

---
+++ Fundamentos do Java/Fluxos/je11_controle_fluxo_excecao/ExemploExcecao.java
@@ -0,0 +1,23 @@
+package je11_controle_fluxo_excecao;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        try {
+            DecimalFormat df = new DecimalFormat();
+            DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+            symbols.setDecimalSeparator(',');
+            symbols.setGroupingSeparator('.');
+            df.setDecimalFormatSymbols(symbols);
+            Number num = df.parse("1,75");
+
+            System.out.println(num.doubleValue());
+        }catch (ParseException e) {
+            System.err.println("Número Inválido");
+        }
+    }
+}
