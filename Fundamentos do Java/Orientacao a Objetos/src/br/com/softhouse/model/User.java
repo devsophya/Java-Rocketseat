54a00736884ffd3464e76b5ada9c52041a73a01a
Sophya <148731999+devsophya@users.noreply.github.com>
2024-12-18 10:23:44 -0300

    pacoteXimportacoes - concluído com êxito.
    pacoteXimportacoes - concluído com êxito.

---
+++ Fundamentos do Java/Orientacao a Objetos/src/br/com/softhouse/model/User.java
@@ -0,0 +1,12 @@
+package br.com.softhouse.model;
+
+import java.math.BigDecimal;
+
+public class User {
+    public static void main(String[] args) {
+        String nome = "sophya";
+        Integer idade = 10;
+        BigDecimal altura = BigDecimal.ONE;
+        System.out.println(altura);
+    }
+}
